40d79916cc03ccf926f8ab9a96946fbb6c9297df
Ajith George Sam <121044173+Ajith-Geo@users.noreply.github.com>
2025-02-16 13:36:43 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const twilio = require("twilio");
 
-const accountSid = "AC967a7aee91b0ca9cf47d299f696777d3"; // Twilio Account SID
-const authToken = "1e18ffb5985f99525a05bd9487be5f6d"; // Twilio Auth Token
-const twilioNumber = "+18572715353"; // Your Twilio Phone Number
+const accountSid = ""; // Twilio Account SID
+const authToken = ""; // Twilio Auth Token
+const twilioNumber = ""; // Your Twilio Phone Number
 
 const client = twilio(accountSid, authToken);
 
@@ -10,7 +10,7 @@ async function createCall() {
   try {
     const call = await client.calls.create({
       from: twilioNumber,
-      to: "+916303105509", // Replace with the actual number
+      to: "+91", // Replace with the actual phone number
       record: true,
       url: "https://7b87-13-202-216-63.ngrok-free.app/twiml-response",
       method:"GET", // FastAPI endpoint for TwiML
